cf5b88cc342c7d1fddd2f1f3e285dd6abe7976d6
amanda-bann <amanda_klap@protonmail.com>
2025-03-03 17:23:33 -0700

    Fetch and display user information

--- js/script.js
+++ js/script.js
@@ -7,8 +7,26 @@ const username = "amanda-bann";
 const getGitProfile = async function () {
     const userData = await fetch(`https://api.github.com/users/${username}`);
     const profile = await userData.json();
-    return profile;
-    // displayUserData(profile);
+    console.log(profile);
+    // return profile;
 };
 
-console.log(getGitProfile());
+getGitProfile();
+
+// Fetch and display user data
+const showUserData = function (profile) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+    <figure>
+      < img alt="user avatar" src=${data.avatar_url} />
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name}</p>
+        <p><strong>Bio:</strong> ${data.bio}</p>
+        <p><strong>Location:</strong> ${data.location}</p>
+        <p><strong>Number of public repos:</strong> ${data.repo_urls}</p>
+    </div>
+    `;
+    overview.append(div);
+};
